088b9df5a7743f5e91c09cab087723736f0aca7c
Lucas Vallejo <lucasvallejo.usa@gmail.com>
2025-03-04 23:46:24 +0100

    feat: add mdx editor toolbar

--- src/renderer/src/components/MarkdownEditor.tsx
+++ src/renderer/src/components/MarkdownEditor.tsx
@@ -1,10 +1,13 @@
 import {
-  MDXEditor,
+  BoldItalicUnderlineToggles,
   headingsPlugin,
   listsPlugin,
   markdownShortcutPlugin,
-  quotePlugin
+  MDXEditor,
+  quotePlugin,
+  toolbarPlugin
 } from '@mdxeditor/editor'
+import '@mdxeditor/editor/style.css'
 import { useMarkdownEditor } from '@renderer/hooks/useMardownEditor'
 
 export const MarkdownEditor = (): JSX.Element => {
@@ -17,7 +20,16 @@ export const MarkdownEditor = (): JSX.Element => {
       markdown={''}
       onChange={handleAutoSaving}
       onBlur={handleBlur}
-      plugins={[headingsPlugin(), listsPlugin(), quotePlugin(), markdownShortcutPlugin()]}
+      plugins={[
+        headingsPlugin(),
+        listsPlugin(),
+        quotePlugin(),
+        markdownShortcutPlugin(),
+        toolbarPlugin({
+          toolbarClassName: 'bg-gray-800',
+          toolbarContents: () => <BoldItalicUnderlineToggles />
+        })
+      ]}
       contentEditableClassName="outline-none min-h-screen max-w-none text-lg px-8 py-5 caret-yellow-500 prose prose-invert prose-p:my-3 prose-p:leading-relaxed prose-headings:my-4 prose-blockquote:my-4 prose-ul:my-2 prose-li:my-0 prose-code:px-1 prose-code:text-red-500 prose-code:before:content-[''] prose-code:after:content-['']"
     />
   )
